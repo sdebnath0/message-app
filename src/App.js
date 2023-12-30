bb2f7a0ae9b4c80694e49fdc962b839e171b62a3
sdebnath0 <soumikdebnath123@outlook.com>
2023-12-29 23:00:26 -0500

    Commit the sidebar in messages

--- src/App.js
+++ src/App.js
@@ -27,12 +27,14 @@ export default App;*/
 
 //this pulls from the pages/register folder
 
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
 import Register from "./pages/Register"
 import "./style.scss"
 
 function App(){
   return(
-    <Register/>
+    <Home/>
   );
 }
 
